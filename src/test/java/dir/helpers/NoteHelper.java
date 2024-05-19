c3467f65db025be5d8cd6ae0a35c55a451940201
lismagilova <gismagilova402@gmail.com>
2024-05-19 17:37:25 +0300

    Add edit assert

--- src/test/java/dir/helpers/NoteHelper.java
+++ src/test/java/dir/helpers/NoteHelper.java
@@ -3,6 +3,9 @@ package dir.helpers;
 import dir.ApplicationManager;
 import dir.model.NoteData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
 import java.util.List;
 
 
@@ -15,13 +18,25 @@ public class NoteHelper extends HelperBase {
         driver.findElement(By.cssSelector("div:nth-child(1) > a:nth-child(8)")).click();
     }
 
-    public void EditNote(NoteData note) {
+    public void EditNote(NoteData note, String newText) {
         driver.findElement(By.cssSelector("#noteid34836 .full")).click();
-        driver.findElement(By.cssSelector("#noteid34836 .full")).sendKeys("Первая запись!\\n");
+        driver.findElement(By.cssSelector("#noteid34836 .full")).clear();
+        driver.findElement(By.cssSelector("#noteid34836 .full")).sendKeys(newText);
     }
 
     public int getNotesCount() {
         List notes = driver.findElements(By.cssSelector(".notes-cont"));
         return notes.size();
+    }
+
+    public String getNoteText() {
+        WebElement noteElement = driver.findElement(By.cssSelector("#noteid34836 .full"));
+
+        // Используем JavaScript для получения значения
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String noteText = (String) js.executeScript("return arguments[0].value;", noteElement);
+
+//        System.out.println("новый текст:" + noteText);
+        return noteText;
     }
 }
--- src/test/java/dir/tests/EditTest.java
+++ src/test/java/dir/tests/EditTest.java
@@ -4,17 +4,25 @@ import dir.model.AccountData;
 import dir.model.NoteData;
 import org.junit.Test;
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
 
 public class EditTest extends TestBase{
     @Test
     public void editNote() {
+        String newNoteText = "Новый текст заметки";
+
         try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         NoteData note = new NoteData("");
-        app.getNote().EditNote(note);
+        app.getNote().EditNote(note, newNoteText);
+
+        String editedNoteText = app.getNote().getNoteText();
+
+        Assert.assertEquals("Заметка не была отредактирована", newNoteText, editedNoteText);
     }
 }
 
